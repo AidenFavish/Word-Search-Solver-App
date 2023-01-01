7679d2d8d16ca85be5ece605a30a31b3fe2a95ef
Aiden F <aidenfavish@gmail.com>
2022-12-31 20:47:05 -0800

    Initial Commit

---
+++ WordSearchSolver2.0/WordSearchSolver2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  WordSearchSolver2_0App.swift
+//  WordSearchSolver2.0
+//
+//  Created by Aiden Favish on 12/31/22.
+//
+
+import SwiftUI
+
+@main
+struct WordSearchSolver2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
